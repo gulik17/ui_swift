a2c0139f9a2300749bafdb9391dd2923beca3aad
Gulik17 <gulik.v@bk.ru>
2019-11-23 19:55:15 +0300

    Lesson 1 Home Work

---
+++ vk/vkontakte/vkontakte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkontakte
+//
+//  Created by Администратор on 23.11.2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
