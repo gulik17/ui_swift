4b16dc04613c7050a2349d0a93fa34bb5cb79f63
Gulik17 <gulik.v@bk.ru>
2019-12-01 19:21:34 +0300

    Доделал ДЗ как надо

--- vk/vkontakte/vkontakte/ViewController.swift
+++ vk/vkontakte/vkontakte/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    
+    
     @IBAction func loginButtonAction(_ sender: Any) {
         if (loginTextField.text == "Admin" && passwordTextField.text == "000123") {
             performSegue(withIdentifier: "loginSuccess", sender: sender)
@@ -20,22 +22,20 @@ class ViewController: UIViewController {
             showAlert(title: "Error", message: "Login or Password is wrong", titleAction: "OK")
         }
     }
-    
-    
+
     @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         registerForKeyboardNotifications()
-        
         let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(hideAction)
     }
-    
+
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
-    
+
     deinit {
         removeKeyboardNotification()
     }
@@ -44,18 +44,18 @@ class ViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
+
     func removeKeyboardNotification () {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
+
     @objc func kbWillShow(_ notification: Notification) {
         let userInfo = notification.userInfo
         let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         scrollView.contentInset.bottom = kbFrameSize.height
     }
-    
+
     @objc func kbWillHide() {
         scrollView.contentInset.bottom = 0
     }
